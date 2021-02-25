58858e244d255db42f44b254ea4b5a586da66155
tiagosgomes <tiagosgtech@outlook.com>
2021-02-25 17:23:56 -0300

    DepartmentList Controller Created!

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -43,7 +43,7 @@ public class MainViewController implements Initializable {
 	}
 
 	@Override
-	public void initialize(URL uri, ResourceBundle rb) {
+	public void initialize(URL url, ResourceBundle rb) {
 
 	}
 
